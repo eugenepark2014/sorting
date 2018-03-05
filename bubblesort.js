438a419113e165d06c7b28ebd9e910d2d18a5360
Jason Y <jasonjyang1@gmail.com>
2018-03-05 13:01:09 -0500

    added check

--- bubblesort.js
+++ bubblesort.js
@@ -1,18 +1,23 @@
-function bubblesort(array){
+let check = true
+
+function bubbleSort(array){
   // console.log(array);
   let temp = array;
   for(let i = 0;i<array.length -1;i++){
-    // console.log(array);
-    swap(temp, i, i+1);
-    console.log(temp);
+    for (let j = 0; j<array.length -1; j++) {
+      swap(temp, j, j+1)
+    }
+    if (check === true) break
   }
-  return array;
+  return temp;
 }
+
 function swap(arr, i, j){
   if(arr[i] > arr[j]){
   const oldVal1 = arr[i];
   arr[i] = arr[j];
   arr[j] = oldVal1;
+  check = false
   }
 }
 // let test = [5,1,2];
--- bubblesort.spec.js
+++ bubblesort.spec.js
@@ -1,17 +1,25 @@
-// describe('Bubble Sort', function(){
-//     it('handles an empty array', function(){
-//       expect( bubbleSort([]) ).toEqual( [] );
-//     });
-//   });
+beforeAll(function () {
+    spyOn(window, swap).and.callThrough(); 
+  });
+  it('getting to the center of tootsiepop involves exactly three licks', function () {
+    
+    expect(window.swap.count()).toEqual(3);
+  });
 
-//   describe('Bubble Sort', function(){
-//     it('can sort numbers', function(){
-//       expect( bubbleSort([3,2,1,99,0]) ).toEqual( [0,1,2,3,99] );
-//     });
-//   });
+describe('Bubble Sort', function(){
+    it('handles an empty array', function(){
+      expect( bubbleSort([]) ).toEqual( [] );
+    });
+  });
 
-//   describe('Bubble Sort', function(){
-//     it('handles an array with numbers only', function(){
-//       expect( bubbleSort([0,100,99,12, null]) ).toEqual( [undefined] );
-//     });
-//   });
+  describe('Bubble Sort', function(){
+    it('can sort numbers', function(){
+      expect( bubbleSort([3,2,1,99,0]) ).toEqual( [0,1,2,3,99] );
+    });
+  });
+
+  describe('Bubble Sort', function(){
+    it('handles an array with numbers only', function(){
+      expect( bubbleSort([0,100,99,12, null]) ).toEqual( [undefined] );
+    });
+  });
