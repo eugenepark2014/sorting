6993a687922c764bfebfe648ee82fc2124a5883e
eugenepark2014 <eugenepark2014@gmail.com>
2018-03-05 14:54:31 -0500

    adding files

--- bubblesort.js
+++ bubblesort.js
@@ -1,26 +1,54 @@
-let check = true
+
+// function bubbleSort(array){
+//   // console.log(array);
+//   let temp = array;
+//   let check = true
+//   while(check){
+//   // for(let i = 0;i<array.length -1;i++){
+//     check = false;
+//     for (let i = 0; i<array.length -1; i++) {
+//       swap(temp, i, i+1, check)
+//     }
+//   }
+//   return temp;
+//   }
+
+// function swap(arr, i, j, check){
+//   if(arr[i] > arr[j]){
+//   const oldVal1 = arr[i];
+//   arr[i] = arr[j];
+//   arr[j] = oldVal1;
+//   check = true;
+//   console.log(check);
+//   }
+// }
+
+
 
 function bubbleSort(array){
   // console.log(array);
+  // while(check){
+  // for(let i = 0;i<array.length -1;i++){
+    // check = false;
   let temp = array;
-  for(let i = 0;i<array.length -1;i++){
-    for (let j = 0; j<array.length -1; j++) {
-      swap(temp, j, j+1)
+  let counter;
+  while(counter >0){
+    counter = 0;
+    for (let i = 0; i<array.length -1; i++) {
+      if(temp[i]>temp[i+1]){
+      swap(temp, i)
+      counter++;
+      }
     }
-    if (check === true) break
   }
-  return temp;
+    return temp;
+  }
+
+function swap(arr, i){
+  const oldHold = arr[i+1];
+  arr[i+1] = arr[i]
+  arr[i] = oldHold
 }
 
-function swap(arr, i, j){
-  if(arr[i] > arr[j]){
-  const oldVal1 = arr[i];
-  arr[i] = arr[j];
-  arr[j] = oldVal1;
-  check = false
-  }
-}
-// let test = [5,1,2];
-// bubblesort(test);
-// swap(test, 0, 1);
-
+var test = [2,3,1]
+console.log(bubbleSort(test));
--- bubblesort.spec.js
+++ bubblesort.spec.js
@@ -1,10 +1,10 @@
-beforeAll(function () {
-    spyOn(window, swap).and.callThrough(); 
-  });
-  it('getting to the center of tootsiepop involves exactly three licks', function () {
-    
-    expect(window.swap.count()).toEqual(3);
-  });
+// beforeAll(function () {
+//     spyOn(window, swap).and.callThrough();
+//   });
+//   it('getting to the center of tootsiepop involves exactly three licks', function () {
+
+//     expect(window.swap.count()).toEqual(3);
+//   });
 
 describe('Bubble Sort', function(){
     it('handles an empty array', function(){
@@ -19,7 +19,9 @@ describe('Bubble Sort', function(){
   });
 
   describe('Bubble Sort', function(){
-    it('handles an array with numbers only', function(){
-      expect( bubbleSort([0,100,99,12, null]) ).toEqual( [undefined] );
+    it('longer array', function(){
+      expect( bubbleSort([5,3,4,1,10,94,0,16,2]) ).toEqual( [] );
     });
   });
+
+  // Expected [ 2, 1, 3, 0, 99 ] to equal [ 0, 1, 2, 3, 99 ].
